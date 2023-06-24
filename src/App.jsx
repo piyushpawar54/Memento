3624af739d5e17005c9ceb5c85c68f07cca3957b
Piyush Pawar <piyushpawar199954@gmail.com>
2023-06-24 20:25:55 +0530

    Game UI Done.

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import Card from './components/Card';
+import shuffle from './utilities/shuffle';
+
+function App() {
+  const [cards, setCards] = useState(shuffle); // Cards array from assets
+
+  return (
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card key={id} image={image} selected={false} onClick={() => {}} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
